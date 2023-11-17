b465428ccee1a0fc9fa6a091d3764b90a0c23201
Beatriz <bea.beatrizmendes60@gmail.com>
2023-11-17 11:34:32 -0300

    global and adm context finished

---
+++ src/providers/AdmContext/@types.ts
@@ -0,0 +1,35 @@
+import { DeepPartial } from "react-hook-form"
+import { IHotel, IBedroom, IPromotion, IImage, IService, IReservation, IUser } from "../../providers/GlobalContext/@types"
+
+export interface IAdmProviderProps {
+    children: React.ReactNode
+}
+
+export type THotelPartial = DeepPartial<IHotel>
+export type TBedroomPartial = DeepPartial<IBedroom>
+export type TPromotionPartial = DeepPartial<IPromotion>
+export type TImagePartial = DeepPartial<IImage>
+export type TServicePartial = DeepPartial<IService>
+
+export interface IAdmContext{
+    createHotel: (formData: THotelPartial) => void
+    updateHotel: (formData: THotelPartial, hotel_id: string) => void
+    deleteHotel: (hotel_id: string) => void
+    isEditHotelOpen: boolean
+    setIsEditHotelOpen: React.Dispatch<React.SetStateAction<boolean>>
+    createRoom: (formData: TBedroomPartial, hotel_id: string) => void
+    updateRoom: (formData: TBedroomPartial, room_id: string, hotel_id: string) => void
+    isEditRoomOpen: boolean
+    setIsEditRoomOpen: React.Dispatch<React.SetStateAction<boolean>>
+    createPromotion: (formData: TPromotionPartial, hotel_id: string) => void
+    getPromotion: () => void
+    deletePromotion: (promotion_id: string) => void
+    getAllReservations: () => void
+    AllReservations: IReservation[]
+    setAllReservations: React.Dispatch<React.SetStateAction<IReservation[]>>
+    getAllUsers: () => void
+    UserList: IUser[]
+    setUserList: React.Dispatch<React.SetStateAction<IUser[]>>
+    createService: (formData: string) => void
+    deleteService: (service_id: string) => void
+}
--- src/providers/GlobalContext/GlobalContext.tsx
+++ src/providers/GlobalContext/GlobalContext.tsx
@@ -4,16 +4,20 @@ import { api } from "../../services/api";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { TLogin } from "../../components/Login/LoginSchema";
-import { IGlobalContext, IGlobalProviderProps, ICurrentUser, IHotel } from "./@types";
+import { IGlobalContext, IGlobalProviderProps, ICurrentUser, IHotel, IBedroom } from "./@types";
 import { TGuestRegisterSchema } from "../../components/Register/RegisterSchema";
+import { TReservationSchema } from "../../components/Reservations/ReservationSchema";
 
 export const GlobalContext = createContext({} as IGlobalContext);
 
 export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
+    const token = localStorage.getItem('user@TOKEN')
     const [CurrentUser, setCurrentUser] = useState<ICurrentUser | null>(null)
     const [HotelsList, setHotelsList] = useState<IHotel[]>([])
     const [Hotel, setHotel] = useState<IHotel | null>(null)
     const [SuggestedHotels, setSuggestedHotels] = useState<IHotel[]>([])
+    const [BedroomsList, setBedroomsList] = useState<IBedroom[]>([])
+
     const navigate = useNavigate();
 
     const login = async (formData: TLogin) => {
@@ -36,7 +40,6 @@ export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
             toast.error('Email invalido');
         }
     }
-
     const logOut = () => {
         localStorage.removeItem('user@TOKEN');
         localStorage.removeItem('user@ID');
@@ -66,6 +69,32 @@ export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
         }
        
     }
+
+    const updateUser = async (formData: TGuestRegisterSchema, user_id: string) => {
+        try{
+            await api.put(`/users/${user_id}`, formData,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('Conta atualizada com sucesso!')
+        }catch(err){
+            toast.error(`${err}`)
+        }
+    }
+    const deleteUser = async (user_id: string) => {
+        try{
+            await api.delete(`/users/${user_id}`,{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('Conta excluída com sucesso!')
+        }catch(err){
+            toast.error(`${err}`)
+        }
+    }
     const getAllHotels = async () => {
         const { data } = await api.get<IHotel[]>('/hotels');
         setHotelsList(data)
@@ -81,9 +110,47 @@ export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
         }
     }
 
+    const getAllBedrooms = async () => {
+        const { data } = await api.get<IBedroom[]>('/bedrooms');
+        setBedroomsList(data)
+    }
+
+    //Reservation Requests
+    const createReservation = async (formData: TReservationSchema) => {
+        try {
+            const { data } = await api.post('/bookings', formData,{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('Reserva efetuada com sucesso!')
+            return data
+            
+        } catch (error) {
+            toast.error(`${error}`)	            
+        }
+    }
+    const updateReservation = async (formData:string, booking_id: string) => {
+        try {
+            const { data } = await api.patch(`/bookings/${booking_id}`, formData,{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('Reserva atualizada com sucesso!')
+
+            return data
+            
+        } catch (error) {
+            toast.error(`${error}`)	            
+        }
+    }
+
+
     useEffect(() => {
         try{
             getAllHotels();
+            getAllBedrooms();
         }catch{
             toast.error('Não foi possível buscar os hoteis')
         }
@@ -101,14 +168,22 @@ export const GlobalProvider = ({ children }: IGlobalProviderProps) => {
             login,
             logOut,
             createUser,
+            updateUser,
+            deleteUser,
             getAllHotels,
             getHotelById,
             CurrentUser,
             HotelsList,
+            setHotelsList,
             Hotel,
             setHotel,
             SuggestedHotels,
-            setSuggestedHotels
+            setSuggestedHotels,
+            BedroomsList,
+            setBedroomsList,
+            getAllBedrooms,
+            createReservation,
+            updateReservation
 
         }}>
             {children}
